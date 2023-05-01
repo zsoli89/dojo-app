ecc685806918db232d5bf15078e85ec8a586003b
zsoli89 <jakabzsoli89@gmail.com>
2023-05-01 16:40:47 +0200

    add: carousel, begining of mainblog

---
+++ src/layouts/DojoPage/DojoLayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DojoLayout = () => {
+  return (
+    <div>DojoLayout</div>
+  )
+}
+
+export default DojoLayout
---
+++ src/layouts/DojoPage/DojoPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DojoPage = () => {
+  return (
+    <div>DojoPage</div>
+  )
+}
+
+export default DojoPage
